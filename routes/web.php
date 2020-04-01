1896c897b645885dfa05f8cef133322cb4b77632
Henry Lee <henryleeworld@gmail.com>
2020-04-01 23:38:14 +0800

    feat:升級至 laravel 7

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::get('users/import/','UsersController@import');
 Route::get('users/export/', 'UsersController@export');
